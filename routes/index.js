d1e232cf470cadd2b0c90d0fde2a25a3f24e0dd1
Elif Balci <mrselifbalci@gmail.com>
2022-11-08 16:59:37 +0000

    title changed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'UniLife' });
+  res.render('index', { title: 'CineTrail' });
 });
 
 module.exports = router;
